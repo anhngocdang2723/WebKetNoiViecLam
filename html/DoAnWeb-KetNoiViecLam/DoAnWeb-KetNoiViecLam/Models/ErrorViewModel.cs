290a881d7c8b2a9ba76d0826769fe1c4e975bd7a
AnhNgocDang <111883222+anhngocdang2723@users.noreply.github.com>
2024-01-01 22:21:24 +0700

    update 1/1.2

--- html/DoAnWeb-KetNoiViecLam/DoAnWeb-KetNoiViecLam/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DoAnWeb_KetNoiViecLam.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- html/README.md
+++
@@ -1,2 +0,0 @@
-# DoAnWeb-KetNoiViecLam
- 
